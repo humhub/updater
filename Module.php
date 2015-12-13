82776ddc7967dd8895ec937c0157c80b70db1ef0
Lucas Bartholemy <lucas@bartholemy.com>
2015-12-13 23:09:40 +0100

    Fixed: Invalid package install

--- Module.php
+++ Module.php
@@ -35,31 +35,34 @@ class Module extends \humhub\components\Module
 
     public function getCurlOptions()
     {
+        // Compatiblity for older versions
+        if (!class_exists('humhub\libs\CURLHelper')) {
+            $options = array(
+                CURLOPT_SSL_VERIFYPEER => (Yii::$app->getModule('admin')->marketplaceApiValidateSsl) ? true : false,
+                CURLOPT_SSL_VERIFYHOST => (Yii::$app->getModule('admin')->marketplaceApiValidateSsl) ? 2 : 0,
+                CURLOPT_REDIR_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
+                CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
+                CURLOPT_CAINFO => Yii::getAlias('@humhub/config/cacert.pem')
+            );
 
-        $options = array(
-            CURLOPT_SSL_VERIFYPEER => (Yii::$app->getModule('admin')->marketplaceApiValidateSsl) ? true : false,
-            CURLOPT_SSL_VERIFYHOST => (Yii::$app->getModule('admin')->marketplaceApiValidateSsl) ? 2 : 0,
-            CURLOPT_REDIR_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
-            CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
-            CURLOPT_CAINFO => Yii::getAlias('@humhub/config/cacert.pem')
-        );
+            if (Setting::Get('enabled', 'proxy')) {
+                $options[CURLOPT_PROXY] = Setting::Get('server', 'proxy');
+                $options[CURLOPT_PROXYPORT] = Setting::Get('port', 'proxy');
+                if (defined('CURLOPT_PROXYUSERNAME')) {
+                    $options[CURLOPT_PROXYUSERNAME] = Setting::Get('user', 'proxy');
+                }
+                if (defined('CURLOPT_PROXYPASSWORD')) {
+                    $options[CURLOPT_PROXYPASSWORD] = Setting::Get('pass', 'proxy');
+                }
+                if (defined('CURLOPT_NOPROXY')) {
+                    $options[CURLOPT_NOPROXY] = Setting::Get('noproxy', 'proxy');
+                }
+            }
 
-
-        if (Setting::Get('enabled', 'proxy')) {
-            $options[CURLOPT_PROXY] = Setting::Get('server', 'proxy');
-            $options[CURLOPT_PROXYPORT] = Setting::Get('port', 'proxy');
-            if (defined('CURLOPT_PROXYUSERNAME')) {
-                $options[CURLOPT_PROXYUSERNAME] = Setting::Get('user', 'proxy');
-            }
-            if (defined('CURLOPT_PROXYPASSWORD')) {
-                $options[CURLOPT_PROXYPASSWORD] = Setting::Get('pass', 'proxy');
-            }
-            if (defined('CURLOPT_NOPROXY')) {
-                $options[CURLOPT_NOPROXY] = Setting::Get('noproxy', 'proxy');
-            }
+            return $options;
         }
-
-        return $options;
+        
+        return \humhub\libs\CURLHelper::getOptions();
     }
 
 }
