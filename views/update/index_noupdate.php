99ffabfcb4df7bb55c4cd204bd16f32217934c64
Lucas Bartholemy <lucas@bartholemy.com>
2021-02-05 14:22:46 +0100

    Add beta channel hint

--- views/update/index_noupdate.php
+++ views/update/index_noupdate.php
@@ -1,14 +1,33 @@
+<?php
+
+/** @var \humhub\modules\updater\Module $module */
+$module = Yii::$app->getModule('updater');
+
+
+?>
 <div class="panel panel-default">
     <div class="panel-heading"><?php echo Yii::t('UpdaterModule.base', '<strong>Update</strong> HumHub'); ?></div>
     <div class="panel-body">
-        
-        <?php echo Yii::t('UpdaterModule.base', 'There is no new HumHub update available!'); ?>
-        <br /><br />
+        <p><?= Yii::t('UpdaterModule.base', 'There is no new HumHub update available!'); ?></p>
+        <p><?= Yii::t('UpdaterModule.base', 'Current update channel: {updateChannel}', [
+                'updateChannel' => '<strong>' . $module->getUpdateChannelTitle() . '</strong>'
+            ]); ?></p>
 
-        <?= Yii::t('UpdaterModule.base', 'Current update channel: {updateChannel}', [
-            'updateChannel' => '<strong>' . Yii::$app->getModule('updater')->getUpdateChannelTitle() . '</strong>'
-        ]); ?>
-        <?= \humhub\libs\Html::a(Yii::t('UpdaterModule.base', 'Change'), \yii\helpers\Url::to(['/updater/admin']), ['class' => 'btn btn-primary btn-sm'] ); ?>
+        <br/>
+
+        <?php if ($module->getUpdateChannel() === 'stable'): ?>
+
+            <div class="alert alert-success">
+                <?= \humhub\libs\Html::a(Yii::t('UpdaterModule.base', 'Updater Configuration'), \yii\helpers\Url::to(['/updater/admin']), ['class' => 'btn btn-success pull-right']); ?>
+
+                <strong><?= Yii::t('UpdaterModule.base', 'Enable Beta Updates'); ?></strong><br/>
+
+                <p>
+                    <?= Yii::t('UpdaterModule.base', 'Change the Update Channel in order to be able to upgrade to beta versions.'); ?>
+                </p>
+            </div>
+        <?php endif; ?>
+
     </div>
 
 </div>
